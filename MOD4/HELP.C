2ea515a7375fda07885d57b8e794ba7e8f5f4aef
MikeFeyrer <36829461+MikeFeyrer@users.noreply.github.com>
2019-04-25 05:48:56 -0400

    Fixed help.c
    Changed from char* to void. We're not using anything being returned. We were getting null pointers because help was not updated with new commands while NUM_CMDS increased.

--- MOD4/HELP.C
+++ MOD4/HELP.C
@@ -10,41 +10,38 @@
 char *helpList[] =
 {
     "Help List\n",
-    "---------------------------------------------\n",
-    "help [command]     - Print command help\n",
-    "version            - MPX version number\n",
-    "date [mm/dd/yy]    - View/change system date\n",
-	"quit               - Terminates MPX\n",
-    "directory          - List .mpx files\n",
-    "prompt=string      - Change MPX promt\n",
-	"alias              - Assign string to command\n"
+    "----------------------------------------------------\n",
+    "help [command]       - Print command help\n",
+    "version              - MPX version number\n",
+    "date [mm/dd/yy]      - View/change system date\n",
+	"quit                 - Terminates MPX\n",
+    "directory            - List .mpx files\n",
+    "prompt=string        - Change MPX promt\n",
+	"alias                - Assign string to command\n",
+    "show command         - Prints list of PCBs\n",
+    "allocate data-x6     - Makes PCB with requested data\n",
+    "free name            - Frees PCB with matching name\n",
+    "load name [ppp]      - Makes PCB of name & priority\n",
+    "resume name          - Makes PCB not suspended\n",
+    "run name [ppp]       - Makes PCB and sets to ready\n",
+    "suspend name         - Suspends PCB called name\n",
+    "terminate name       - Terminates PCB called name\n",
+    "setpriority name ppp - Sets PCB's priority\n",
+    "dispatch             - Dispatches a process\n"
 };
 
 // takes a command to print help information about -
 // returns a string, if no command given returns all
-char* help(char* args, int argc, int cmdMatch){
-	char* helpOut;
+void help(char* args, int argc, int cmdMatch){
 	int i;
 	// if number of arguments is greater than two, invalid
-	if (argc > 2)
-	{
-	printf("Too many arguments. Commands used: %d. Please use only one command. \n", argc);
-	}
-	else if(argc == 1)
-	{
-	for(i = 0; i < NUM_CMDS+2; i++)
-	{
-	   printf("%s", helpList[i]);
-	}
-	}
-	else
-	{
-	   printf("%s",helpList[cmdMatch+2]);
-
-	}
-
-	// if no argument print all help
-
-	// return our string
-	return helpOut;
+	if (argc > 2) {
+        printf("Too many arguments. Commands used: %d. Please use only one command. \n", argc);
+    } else if (argc == 1) {
+        for (i = 0; i < NUM_CMDS+2; i++) {
+            printf("%s", helpList[i]);
+        }
+    } else {
+        printf("%s",helpList[cmdMatch+2]);
+    }
 }
